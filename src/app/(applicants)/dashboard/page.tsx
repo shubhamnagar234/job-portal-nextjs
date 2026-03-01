75123b4672ae8f697fc4e9c775f570459290fbb6
Shubham Nagar <shubhamnagar234@gmail.com>
2026-03-01 20:04:39 +0530

    Route Groups - Clean Folder Structure + Applicant Page Setup

--- src/app/(applicants)/dashboard/page.tsx
+++ src/app/(applicants)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { logoutUserAction } from "@/features/auth/server/auth.actions";
+
 const ApplicantDashboard = () => {
   return (
     <div>
       <h1>Applicant Dashboard</h1>
+      <button onClick={logoutUserAction}>Logout</button>
     </div>
   );
 };
