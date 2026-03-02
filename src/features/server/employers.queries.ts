212867917e4acbc057e00c5ff2b102af32ccecb5
Shubham Nagar <shubhamnagar234@gmail.com>
2026-03-02 19:35:54 +0530

    Employer Profile Status Check

---
+++ src/features/server/employers.queries.ts
@@ -0,0 +1,27 @@
+import { db } from "@/config/db";
+import { getCurrentUser } from "../auth/server/auth.queries";
+import { employers } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+
+export const getCurrentEmployeeDetails = async () => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) return;
+
+  if (currentUser.role !== "employer") return null;
+
+  const [employer] = await db
+    .select()
+    .from(employers)
+    .where(eq(employers.id, currentUser.id));
+
+  console.log(employer);
+
+  const isProfileCompleted =
+    employer?.name &&
+    employer.description &&
+    employer.avatarUrl &&
+    employer.organizationType &&
+    employer.yearOfEstablishment;
+
+  return { ...currentUser, employeeDetails: employer, isProfileCompleted };
+};
